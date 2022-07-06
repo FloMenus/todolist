af16a8bef0c0d2861d771090c479da1d655a25df
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-07-06 17:07:10 +0200

    JS tests

--- index.js
+++ index.js
@@ -13,19 +13,44 @@
 
 // événements
 
-createButton.addEventListener("click", () => {
-  onTaskSubmit("create")
-})
+// createButton.addEventListener("click", () => {
+//   onTaskSubmit("create")
+// })
 
-document.getElementById("create").addEventListener('click',function(e){
-  e.preventDefault(); // Cancel the native event
-  e.stopPropagation();// Don't bubble/capture the event any further
-});
+// document.getElementById("create").addEventListener('click',function(e){
+//   e.preventDefault(); // Cancel the native event
+//   e.stopPropagation();// Don't bubble/capture the event any further
+// });
+
+// const task = {
+//   toDo: "Etudier",
+//   value: "5",
+//   status:"En cours"
+// }
+
+// // fonctions
+// const create = document.getElementById('create')
+
+// const createdClicked = () => {
+//   create.innerHTML = `<p>création!</p>`
+// }
+
+// const test = document.getElementById('test')
+
+// const testClicked = () => {
+//   test.innerHTML = `${task}`
+// }
 
 
 
-// fonctions
+// FONCTIONNE!!! 
+// const test = document.getElementById('create')
 
-const onTaskSubmit = () => {
+// const onTaskSubmit = () => {
+//   create.innerHTML = `<p>test réussi!</p>`
+// }
+// !!!!
 
-}
+
+//  const titre = document.getElementById('titre')
+//   titre.innerHTML = `<h1>Shi-Fu-Mi!!!</h1>`
