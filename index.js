0c3e864af8a0c9843716eff881501fa165acd10b
FloMenus <florent.mns@gmail.com>
2022-07-07 10:01:00 +0200

    added windows opening & closing

--- index.js
+++ index.js
@@ -54,3 +54,44 @@
 
 //  const titre = document.getElementById('titre')
 //   titre.innerHTML = `<h1>Shi-Fu-Mi!!!</h1>`
+
+
+
+//  OUVERTURE/FERMETURE DES FENETRES ///////////////////////////////
+
+let taskCreatorWindow = document.getElementsByClassName("task-creator-window")
+let taskNameModifier = document.getElementsByClassName("task-name-modifier-window")
+let taskPriorityModifier = document.getElementsByClassName("task-priority-modifier-window")
+let taskStatusModifier = document.getElementsByClassName("task-status-modifier-window")
+
+
+taskCreatorWindowOpener = () => {
+    taskCreatorWindow[0].classList.remove("hidden")
+}
+taskCreatorWindowCloser = () => {
+    taskCreatorWindow[0].classList.add("hidden")
+}
+
+
+taskNameWindowOpener=()=> {
+    taskNameModifier[0].classList.remove("hidden")
+}
+taskNameWindowCloser=()=> {
+    taskNameModifier[0].classList.add("hidden")
+}
+
+taskPriorityWindowOpener=()=> {
+    taskPriorityModifier[0].classList.remove("hidden")
+}
+taskPriorityWindowCloser=()=> {
+    taskPriorityModifier[0].classList.add("hidden")
+}
+
+taskStatusWindowOpener=()=> {
+    taskStatusModifier[0].classList.remove("hidden")
+}
+taskStatusWindowCloser=()=> {
+    taskStatusModifier[0].classList.add("hidden")
+}
+
+
